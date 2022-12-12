50705b56bb7acfc6d0271b9149b9a8c15c9213da
umarbshehu <umarbshehu@gmail.com>
2022-12-12 15:24:33 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++);
-		putchar(i);
+	putchar(i);
 
 	for (i = 'A'; i <= 'Z'; i++);
-		putchar(i);
+	putchar(i);
 
 	putchar('\n');
 	return (0);
