ba3392d35ca1767dee9d45ec59e2fd11b7fd09d5
umarbshehu <umarbshehu@gmail.com>
2022-12-12 15:48:22 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reverse order
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc >= 'a'; lc --)
+		putchar(lc);
+	putchar('\n');
+
+	return (0);
+}
