0bca2fd7f39168d9b70658e2657ee51b273c6765
umarbshehu <umarbshehu@gmail.com>
2022-12-12 15:05:51 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Description - Program to print lowcase alphabet
+ *
+ * Return: Alway return 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
