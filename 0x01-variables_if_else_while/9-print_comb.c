e624f210e0b12f6051779a405b5c56536fe40ccc
umarbshehu <umarbshehu@gmail.com>
2022-12-12 16:01:56 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Printing combination of numbers
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main (void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+			if (num < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
