b8852d68ca8cdf52090ce72d0ee0eecd9d764f4f
umarbshehu <umarbshehu@gmail.com>
2022-12-14 13:46:40 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(void)
+{
+	int p[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+
+	return (0);
+}
