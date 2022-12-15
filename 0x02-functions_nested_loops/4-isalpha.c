13fee0dbd3fb51071ebb149ddecfc030e4072266
umarbshehu <umarbshehu@gmail.com>
2022-12-15 08:32:46 -0800

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 
 /**
  *_isalpha - checks if a character is an alphabet
- * @c: the ASCII value of the character to be checked 
- *Return: the program returns 0 if successful 
+ * @c: the ASCII value of the character to be checked
+ *Return: the program returns 0 if successful:wq!
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90) || (c >= 97 && c<= 122)
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
 	else
 		return (0);
