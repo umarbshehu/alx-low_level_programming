3a80f1a668913e5c29ebf1a750293eed5d436cbe
umarbshehu <umarbshehu@gmail.com>
2022-12-15 08:24:51 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - checks for lowercase characters
- * @c: The character to be checked
+ *@c: single letter input
  * Return: the program returns 0 on success
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
