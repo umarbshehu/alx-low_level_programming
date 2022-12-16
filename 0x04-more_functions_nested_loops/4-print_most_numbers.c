2a89616b77e4d82c5352251a1f3b51cb1ddca73c
umarbshehu <umarbshehu@gmail.com>
2022-12-16 01:29:08 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,13 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if ((i == 2) || (i == 4)
-			continue
+		if (i == 2 || i == 4)
+			continue;
 		else
 
-			_putchar(i =
+			_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
 
