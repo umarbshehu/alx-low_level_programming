421181be2e95231f62c158c94e733538890b69b1
umarbshehu <umarbshehu@gmail.com>
2022-12-21 06:22:27 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: copt to
+ * @src: copu from
+ * Return: two concat strings
+ */
+
+char *_str(char *dest, char *src)
+{
+	int i, n;
+
+	for (i = 0, dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
+}
