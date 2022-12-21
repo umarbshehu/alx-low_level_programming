e3108ffbd390fed31b4aef6f20e26f0e7722a0a0
umarbshehu <umarbshehu@gmail.com>
2022-12-21 14:29:43 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char s1[98] = "Hello ";
-	char s2[] = "World! \n";
+	char s2[] = "World!\n";
 	char *ptr;
 
 	printf("%s\n", s1);
--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,23 @@
 
 /**
  * _strcat - a function that concatenates two strings
- * @dest: copt to
- * @src: copu from
+ * @dest: copy to
+ * @src: copy from
  * Return: two concat strings
  */
 
-char *_str(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i, n;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
+
 	}
 
 	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
 	{
+
 	}
 
 	return (dest);
