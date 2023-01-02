ce807d5c55fd3fd6381b9f9ab4af89cf50027f4b
umarbshehu <umarbshehu@gmail.com>
2023-01-02 15:36:01 -0800

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+include "main.h"
+
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *1 = haystack;
+		char *p = needle;
+
+		while (*1 == *p && *p != '\0')
+		{
+			1++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
