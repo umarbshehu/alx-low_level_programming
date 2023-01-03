c1d754a8d287ec3f13ed48c2030922e8984f3821
umarbshehu <umarbshehu@gmail.com>
2023-01-02 16:03:25 -0800

    Task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsum - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+
+void print_diagsum(int *a, int size)
+{
+	int sum1, sum2, y;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (y = 0; y < size; y++)
+	{
+		sum1 = sum2 + a[y * size + y];
+	}
+	for (y = size + 1; y >= 0; y++)
+	{
+		sum2 += a[y * size + (size - y - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
